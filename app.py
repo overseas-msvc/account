26cf247d68d9462de9ff472e1f24cf72e81bdd57
hanna <channashosh@gmail.com>
2025-03-09 15:00:42 +0200

    login and signup

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import json
-from flask import Flask, request
+import base64
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from db_manage.mysql_connector.database import Database
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/accounts", methods=["GET"])
 def get_accounts():
@@ -92,6 +95,52 @@ def delete_user():
 	db.delete_object("user", data["id"])
 	return "", 204
 
+@app.route("/login", methods=["POST"])
+def login():
+	data = request.json
+	db = Database("account")
+	user = db.get_list_of_objects("user", {"name": data["username"]})
+	if user:
+		user = user[0]
+	else:
+		user = db.get_list_of_objects("user", {"email": data["username"]})
+		if user:
+			user = user[0]
+		else:
+			return f"no such user {data["username"]}", 405
+	if base64.b64decode(data["password"]) != base64.b64decode(user.encrypted_password):
+		return jsonify({"message": "wrong password"}), 401
+
+	return json.dumps({"userId": user.id, "accountId": user.account_id}), 200
+
+
+@app.route("/signup", methods=["POST"])
+def signup():
+	data = request.json
+	if data["accountName"] == '':
+		return jsonify({"message": f"invalid account name"}), 401
+	if data["username"] == '':
+		return jsonify({"message": f"invalid username"}), 401
+	if data["email"] == '':
+		return jsonify({"message": f"invalid email"}), 401
+	if data["password"] == '':
+		return jsonify({"message": f"invalid password"}), 401
+	db = Database("account")
+	account = db.get_list_of_objects("account", {"name": data["accountName"]})
+	if account:
+		account_id = account[0].id
+	else:
+		account_id = db.add_object("account", {"name": data["accountName"]})
+	user = db.get_list_of_objects("user", {"name": data["username"],
+							 	"account_id": account_id})
+	if user:
+		return jsonify({"message": f"user '{user[0].name}' already exists in account {data["accountName"]}"}), 401
+	user_id = db.add_object("user", data={"name": data["username"],
+							 	"account_id": account_id,
+								"email": data["email"],
+								"encrypted_password": data["password"]})
+	# verify email
+	return {"userId": user_id, "accountId": account_id}
 
 
 if __name__ == "__main__":
